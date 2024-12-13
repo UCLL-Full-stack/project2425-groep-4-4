13a0fee4238a41142884e1c3cc3c12e9e2744c8b
Paul Michiels <114234570+michielspaul@users.noreply.github.com>
2024-12-13 11:32:48 +0100

    responsive header

--- front-end/components/Header.tsx
+++ front-end/components/Header.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="header flex">
+    <header className="header flex wrap">
       <div className="flex logo-container header-section">
         <img className='logo' src="/images/logo.png" alt="Cineflex Logo" />
         <h2>Cineflex</h2>
       </div>
       <nav className="navigation">
-        <ul className='flex'>
+        <ul className='flex wrap'>
           <li><a href="/">All Movies</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#contact">Contact</a></li>

7e57dfc347b41bb16bb35c69119bccadbffe6465
Paul Michiels <114234570+michielspaul@users.noreply.github.com>
2024-12-15 13:52:38 +0100

    check

---
+++ front-end/components/ProgramOverview.tsx
@@ -0,0 +1,42 @@
+import { Voorstelling } from "@/types/types";
+import { Film } from "@/types/types";
+import { useRouter } from "next/router";
+import React from "react";
+
+type Props = {
+    voorstellingen: Array<Voorstelling>;
+};
+
+const ProgramOverview: React.FC<Props> = ({ voorstellingen }: Props) => {
+    const router = useRouter();
+
+    const goToVoorstellingPage = (id: number) => {
+        router.push("/voorstelling/" + id)
+    }
+
+
+    if (!voorstellingen || voorstellingen.length === 0) {
+        return <p>Geen voorstellingen beschikbaar.</p>;
+    }
+    
+    return (
+        <div className="flex wrap center-content container">
+            {voorstellingen.map((voorstelling, index) => (
+                <div
+                    className="box"
+                    onClick={() => goToVoorstellingPage(voorstelling.id)}
+                >
+                    <div className="icon">
+                        <img src={`./images/cover-${voorstelling.film.id}.jpg`} alt="movie cover" />
+                    </div>
+                    <div className="details">
+                        <p>Lounge: {voorstelling.zaal.id}</p>
+                        <p>Date: {voorstelling.datum}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ProgramOverview;

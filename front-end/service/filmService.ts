a55ed4c85410543cba2ff3d594e448771ff6f834
Paul Michiels <114234570+michielspaul@users.noreply.github.com>
2024-12-18 16:06:34 +0100

    completed film and program service

--- front-end/service/filmService.ts
+++ front-end/service/filmService.ts
@@ -1,3 +1,5 @@
+import { Film } from '@/types/types';
+
 const getAllFilms = async () => {
   const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/film/getAll', {
     method: 'GET',
@@ -16,12 +18,25 @@ const getFilmById = async (id: number) => {
     }
   });
   return response;
+};
+
+const createFilm = async (film: Film) => {
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/film/create', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(film)
+  });
+  return response;
 }
+
   
-  const filmService = {
-    getAllFilms,
-    getFilmById,
-  };
-  
-  export default filmService;
+const filmService = {
+  getAllFilms,
+  getFilmById,
+  createFilm
+};
+
+export default filmService;
   
--- front-end/service/voorstellingService.ts
+++ front-end/service/voorstellingService.ts
@@ -1,26 +1,40 @@
+import { Voorstelling } from "@/types/types";
+
 const getAllVoorstellingen = async () => {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/voorstelling/getAll', {
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json',
-      }
-    });
-    return response;
-  };
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/voorstelling/getAll', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+  });
+  return response;
+};
   
-  const getVoorstellingById = async (id: number) => {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/voorstelling/' + id, {
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json',
-      }
-    });
-    return response;
-  }
-    
-    const voorstellingService = {
-      getAllVoorstellingen,
-      getVoorstellingById,
-    };
-    
-    export default voorstellingService;
+const getVoorstellingById = async (id: number) => {
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/voorstelling/' + id, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+  });
+  return response;
+};
+
+const createVoorstelling = async (voorstelling: Voorstelling) => {
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/voorstelling/create', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(voorstelling)
+  });
+  return response;
+}
+
+const voorstellingService = {
+  getAllVoorstellingen,
+  getVoorstellingById,
+  createVoorstelling
+};
+  
+export default voorstellingService;
